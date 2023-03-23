deee956a6cf2f904cd194cf804d7456c2dd519c8
gekoJs <daniroa.js@gmail.com>
2023-03-23 11:45:00 -0400

    deploy

--- cliente/src/components/loader/loader.jsx
+++
@@ -1,9 +0,0 @@
-import style from "./loader.module.scss";
-import paw from "../../assets/svg/Paw.svg";
-export default function Loader() {
-  return (
-    <div className={style.center}>
-        <img className={style.paw} src={paw} alt="" />
-    </div>
-  );
-}
