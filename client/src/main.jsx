bd03b2ec680738241ae1cfac7fcd52a6013b5f09
gekoJs <daniroa.js@gmail.com>
2023-03-23 11:44:03 -0400

    deploy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,8 +7,8 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import axios from "axios"
 
-axios.defaults.baseURL = "http://localhost:3001"
-// axios.defaults.baseURL = "https://pidogsback-production.up.railway.app/"
+// axios.defaults.baseURL = "http://localhost:3001"
+axios.defaults.baseURL = "https://pidogs-production-41b4.up.railway.app/"
 
 ReactDOM.render(
   <React.StrictMode>
